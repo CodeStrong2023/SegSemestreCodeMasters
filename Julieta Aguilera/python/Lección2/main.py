b1ade27127a54d6c44162aebd3f589f0c03b3c01
Julieta Aguilera <aguilerajulieta700@gmail.com>
2023-09-22 19:55:01 -0300

    se agregan ejercicios 6, 7, 8 de la leccion1

---
+++ Julieta Aguilera/python/Lección2/main.py
@@ -0,0 +1,10 @@
+# Comenzamos con funciones
+
+# Definimos una función
+# mi_funcion() #No se puede llamar antes de definir a una funcion
+def mi_funcion(): # para identificar a la funcion utilizamos parentesis
+    print('Buenas tardes')
+
+mi_funcion() #estamos llamando la funcion
+mi_funcion() # Se puede llamar a una funcion N cantidad de veces
+

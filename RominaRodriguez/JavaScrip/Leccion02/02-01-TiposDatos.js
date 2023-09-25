1d86726f439a74eb4e659074aef0bcd0298a9e64
romina <romirodrigez13@gmail.com>
2023-09-25 10:16:38 -0300

    Clase 3 tipos de datos en J.S

---
+++ RominaRodriguez/JavaScrip/Leccion02/02-01-TiposDatos.js
@@ -0,0 +1,18 @@
+// Tipos de Datos en JavaScript
+/*
+La sintaxis en lo que es comentarioas 
+es muy similar a la de Java
+realmente diriamos que es identica 
+*/
+var nombre = "Romina"; //Tipo Str
+console.log(nombre);
+
+var numero = 3000;//Tipo númerico
+console.log(numero);
+
+var objeto = {
+    nombre:"Romina",
+    apellido:"Rodriguez",
+    telefono:"2618907766"
+}
+console.log(objeto);

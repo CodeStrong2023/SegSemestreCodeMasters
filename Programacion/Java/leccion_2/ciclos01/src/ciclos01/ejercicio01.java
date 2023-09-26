ff03d29cf32269552998c1b5ab0e1b881cdb724a
Julieta Aguilera <aguilerajulieta700@gmail.com>
2023-09-25 21:18:49 -0300

    se agregan clases de colecciones en pytthon y ejercicios en java

--- Programacion/Java/leccion_2/ciclos01/src/ciclos01/ejercicio01.java
+++ Programacion/Java/leccion_2/ciclos01/src/ciclos01/ejercicio01.java
@@ -7,12 +7,11 @@ public class ejercicio01 {
 
     public static void main(String[] args) {
         int numero, cuadrado;
-        numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un número"));
-        while (numero
-                >= 0) {  //mientra el número sea igual a cero o positivo
+        numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un número: "));
+        while (numero >= 0) {  //mientra el número sea igual a cero o positivo
             cuadrado = (int) Math.pow(numero, 2);
-            System.out.println("el número " + numero + " elevado al cuadrado: " + cuadrado);
-            numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un número"));
+            System.out.println("el número " + numero + " elevado al cuadrado es: " +cuadrado);
+            numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un número: "));
 
         }
         System.out.println("el programa a finalizado por números negativos");

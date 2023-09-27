66f1bf380f3d58164e35302ae80833df70806c07
Julieta Aguilera <aguilerajulieta700@gmail.com>
2023-09-27 18:43:38 -0300

    se agregan ejercicios con ciclos y clases Scanner y JOptionPane

--- Julieta Aguilera/java/leccion4/Aritmetica/src/Operaciones/Aritmetica.java
+++ Julieta Aguilera/java/leccion4/Aritmetica/src/Operaciones/Aritmetica.java
@@ -15,11 +15,11 @@ public class Aritmetica {
     }
     public int sumarConRetorno(){
         //int resultado = a + b;
-        return a + b;
+        return this.a + this.b;
     }
-    public int sumarConArgumentos(int arg1, int arg2){
-        a = arg1;
-        b = arg2;
+    public int sumarConArgumentos(int a, int b){
+        this.a = a; //El argumento a se asigna al atributo this.a
+        this.b = b;
         //return a + b;
         return sumarConRetorno();
     }

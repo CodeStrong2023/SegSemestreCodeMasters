1fd0e6adcc7071245c09ac55bc870249eb723852
Julieta Aguilera <aguilerajulieta700@gmail.com>
2023-09-27 13:52:48 -0300

    Se agregan nuevos ejercicios  en java

--- Julieta Aguilera/java/leccion4/Aritmetica/src/Operaciones/Aritmetica.java
+++ Julieta Aguilera/java/leccion4/Aritmetica/src/Operaciones/Aritmetica.java
@@ -13,4 +13,16 @@ public class Aritmetica {
         int resultado = a + b;
         System.out.println("resultado = "+resultado);
     }
+    public int sumarConRetorno(){
+        //int resultado = a + b;
+        return a + b;
+    }
+    public int sumarConArgumentos(int arg1, int arg2){
+        a = arg1;
+        b = arg2;
+        //return a + b;
+        return sumarConRetorno();
+    }
+        
+    
 }

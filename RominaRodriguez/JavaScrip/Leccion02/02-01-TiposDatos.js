55d8d8a7a1928d544a6c7bc513d7f7136f594525
romina <romirodrigez13@gmail.com>
2023-09-28 11:33:08 -0300

    Clase 4 JavaScript Lección2 Tipos de Datos

--- RominaRodriguez/JavaScrip/Leccion02/02-01-TiposDatos.js
+++ RominaRodriguez/JavaScrip/Leccion02/02-01-TiposDatos.js
@@ -4,15 +4,41 @@ La sintaxis en lo que es comentarioas
 es muy similar a la de Java
 realmente diriamos que es identica 
 */
-var nombre = "Romina"; //Tipo Str
+//Tipo String
+var nombre = "Romina"; 
 console.log(nombre);
+nombre = 7;
+console.log(typeof nombre);
 
-var numero = 3000;//Tipo númerico
+//Tipo númerico
+var numero = 3000;
 console.log(numero);
 
+//Tipo objeto
 var objeto = {
     nombre:"Romina",
     apellido:"Rodriguez",
     telefono:"2618907766"
 }
-console.log(objeto);
+console.log(typeof objeto);
+
+//Tipos de Datos Booleanos
+var bandera = true;
+console.log(typeof bandera);
+
+//Tipo de dato de una Función
+function miFuncion(){}
+console.log(typeof miFuncion);
+
+//Tipo de dato symbol
+var simbolo = Symbol("mi simbolo");
+console.log(typeof simbolo);
+
+//Tipo de dato clase
+class Persona{
+    constructor(nombre,apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;  
+    }
+}
+console.log(Persona);

aaa507fbe9b53febf8fe5b601557b9849c8e9418
Julieta Aguilera <aguilerajulieta700@gmail.com>
2023-10-17 17:02:32 -0300

    se agrgan nuevos ejercicios de POO

---
+++ Julieta Aguilera/java/Proyecto/JuegoAzar/src/juego/AdivinaLaPalabra.java
@@ -0,0 +1,16 @@
+
+package juego;
+
+import java.util.Scanner;
+
+public class AdivinaLaPalabra {
+    public static void main(String[] args){
+    Scanner entrada = new Scanner(System.in);
+    String palabra, pista;
+    palabra = "Pérentesis";
+    System.out.println("Bienvenido a Adivina la palabra"); 
+    System.out.println("La palabra que elijas debes escribirla con la inicial en mayúscula y con su respectivo tilde si lo requiere."); 
+    System.out.println("Pista:  ¿Qué utilizamos para saber que es una Tupla? ");
+    
+    } 
+}

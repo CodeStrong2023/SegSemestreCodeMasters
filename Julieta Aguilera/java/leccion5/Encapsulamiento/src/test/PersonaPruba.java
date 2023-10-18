42af03e89751ed048fd2b6339c4ae9e021d57a67
Julieta Aguilera <aguilerajulieta700@gmail.com>
2023-10-18 17:39:31 -0300

    se agregan nuevos ejercicios en java

--- Julieta Aguilera/java/leccion5/Encapsulamiento/src/test/PersonaPruba.java
+++ Julieta Aguilera/java/leccion5/Encapsulamiento/src/test/PersonaPruba.java
@@ -1,14 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package test;
 
-/**
- *
- * @author Usuario
- */
+import dominio.Persona;
+
 public class PersonaPruba {
-    
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Julieta", 57.000, false);
+        
+     //Modificar a travéz de los metodos   
+     persona1.setNombre("Erica Aguilera");
+     //persona1.nombre = "Erica Aguilera"; //ya no se puede utilizar
+     //System.out.ptintln("Nombre es:"+persona1.nombre); //Error
+        System.out.println("persona1 su nombre modificado es: "+persona1.getNombre());
+        System.out.println("persona1 el resultado para el sueldo es: "+persona1.getSueldo());
+        System.out.println("persona1 para obtener el booleano: "+persona1.isEliminado());
+    }
 }

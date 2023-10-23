7679c715c3c98c3472df76d63767aa843d06b68a
Julieta Aguilera <aguilerajulieta700@gmail.com>
2023-10-23 20:34:13 -0300

    se agrega otra leccion a la clase de javascript

---
+++ Julieta Aguilera/python/JavaScrip/leccion02/02-01-TiposDatos.js
@@ -0,0 +1,23 @@
+//Tipos de datos en javascript
+/*
+La sintaxis en los comentarios 
+es muy similar a la de java
+ */
+var nombre = 'Julieta'; //Tipo Str
+console.log(typeof nombre);
+nombre = 7;
+console.log(nombre);
+
+var numero = 3000; //Tipo numerico
+console.log(typeof numero);
+nombre = 11.3;
+console.log(typeof nombre)
+var objeto = {
+    nombre: "Julieta",
+    apellido: "Aguilera",
+    telefono: 2604341029
+
+}
+console.log(typeof objeto);
+
+//Tipo de dato boolean

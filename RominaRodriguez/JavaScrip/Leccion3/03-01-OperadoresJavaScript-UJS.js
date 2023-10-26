da85f2390afe5498d77acff8bc2f44c1ea09ba55
romina <romirodrigez13@gmail.com>
2023-10-26 16:10:21 -0300

    Clase 7 de JavaScript Leccion 3

---
+++ RominaRodriguez/JavaScrip/Leccion3/03-01-OperadoresJavaScript-UJS.js
@@ -0,0 +1,17 @@
+//Ejercicio para encontrar números pares
+let par_impar = 10;
+if(par_impar % 2 ==0){
+    console.log("Es un número par");
+}
+else{
+    console.log("Es un número impar");
+}
+
+//Ejercicio: es mayor de edad
+let edad = 20, adulto = 18;
+if(edad >= adulto){
+    console.log("es una persona adulta");
+}
+else{
+    console.log(" es menor de edad")
+}

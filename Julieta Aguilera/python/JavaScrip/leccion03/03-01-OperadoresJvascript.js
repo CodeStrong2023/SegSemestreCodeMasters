8e56671b9b645f6e6f6d94e9d2cbd5aa842c1b86
Julieta Aguilera <aguilerajulieta700@gmail.com>
2023-10-27 12:49:20 -0300

    se agregan nuevos ejercicios en javascript

---
+++ Julieta Aguilera/python/JavaScrip/leccion03/03-01-OperadoresJvascript.js
@@ -0,0 +1,17 @@
+//Ejercicio para encontrar numeros pares 
+let parImpar = 10;
+if(parImpar % 2 == 0){
+    console.log("Es un numero Par");
+}
+else{
+    console.log("Es un numero Impar");
+}
+
+//Ejercicio es mayor de edad 
+let edad = 20, adulto = 18;
+if(edad >= adulto){
+    console.log("Eres mayor de edad");
+}
+else{
+    console.log("Eres menor de edad")
+}

dd32ec9e9b9de62cd77f7c9471631993919c5dc2
romina <romirodrigez13@gmail.com>
2023-11-13 15:15:07 -0300

    Clase 10 de JavaScrip Lección 04

--- RominaRodriguez/JavaScrip/Leccion04/04-01-Ejercicios.js
+++ RominaRodriguez/JavaScrip/Leccion04/04-01-Ejercicios.js
@@ -1,52 +1,83 @@
-//Ampliamos el uso de var let y const
-/*
-Con var puedes reasignar en cualquier momento
-este forma parte del ambito global
-Un error es que se sobreescriba
-*/
-
-var nombre = "Ariel";
-nombre = "Osvaldo";
-console.log(nombre);
-
-function saludar(){
-    var nombre3 = "Natalia";
-    console.log(nombre3);
+//Ejercicio 1: Calcular estación del año
+let mes = 12;
+let estacion;
+if(mes == 1 || mes == 2 || mes == 12){
+    estacion = "Verano"
 }
-//console.log(nombre3); //Aquí no lee el dato en la función
-
-if(true){
-    var edad = 34;
-    console.log(edad);
+else if(mes == 2 || mes == 4 || mes == 5){
+    estacion = "Otoño"
 }
-console.log(edad);//En la función funciono correctamente, en la estructura if fallo
-
-/*
-let: esta puede ser reasignada en cualquier momento
-la diferencia es que su ambito es de bloque,
-solo disponible dentro de un bloque de llaves
-o dentro de una función
-*/
-
-function saludar2(){
-    let nombre2 = "Ariel";
-    console.log(nombre2);
+else if(mes == 6 || mes == 7 || mes == 8){
+    estacion = "Invierno"
 }
-//console.log(nombre2);
-
-if(true){
-    let edad2 = 33;
-    console.log(edad2);
+else if(mes == 9 || mes == 10 || mes == 11){
+    estacion = "Primavara"
 }
-//console.log(edad2);
+else{
+    estacion = "Valor incorrecto"
+}
+console.log("El valor corresponde a la estación de : "+estacion);
+
+
+//Ejercicio 2: Hora del día
 
 /*
-const se utiliza para valores constantes que no pueden ser reasignadas
+de 7 a 9 desayuno
+de 10 a 12 preparo el almuerzo
+de 13 a 14 almmorzamos
+de 15 a 17 descanzamos la siesta
+de 18 a 20 hago ejercicio
+de 21 a 22 cena libiana
+de 23 a24 veo una serie
+de 24 a 7 duermo
 */
+let horaDia = 18;
+let mensaje;
+if(horaDia >= 7 && horaDia <=9){
+    mensaje = "Me levanto y desayuno";
+}
+else if(horaDia >= 10 && horaDia <= 12){
+    mensaje ="Preparo el almuerzo";
+}
+else if(horaDia >= 13 && horaDia <= 14){
+    mensaje = "Hora de almorzar";
+}
+else if(horaDia >= 15 && horaDia <=17){
+    mensaje = "Descanso la siesta";
+}
+else if(horaDia >= 18 && horaDia <= 20){
+    mensaje = "Hago ejercicio y ducha";
+}
+else if(horaDia >= 21 && horaDia <= 22){
+    mensaje = "Cena libiana";
+}
+else if(horaDia >= 23 && horaDia <= 24){
+    mensaje= "Veo una serie";
+}
+else if(horaDia >= 24 && horaDia <= 7){
+    mensaje = "Hora de dormir";
+}
+else{
+    mensaje = "Opción no valida";
+}
+console.log("A esta hora del día: "+horaDia+"hs"+" " +mensaje)
 
-const fechaNacimiento = 2006;
-console.log(fechaNacimiento);
-//fechaNacimiento = 2003;
-//console.log(fechaNacimiento);//solo se ejecuta en console anterior
-
+//Estructura switch(la sintaxis es igual a java)
+switch(mes){ //No solo se puede utilizar números, también cadenas
+    case 1: case 2: case 12:
+        estacion = "Verano";
+        break;
+    case 3: case 4: case 5:
+        estacion = "Otoño";
+        break;
+    case 6: case 7: case 8:
+        estacion = "Invierno";
+        break;
+    case 9: case 10: case 11:
+        estacion = "Primavera";
+        break
+    default:
+        estacion = "Vlor incorrecto";                    
+}
+console.log("Bienveidos a la estación de: "+estacion)
 

78fcfdc9ba967a15133fb0ea38c629b578f54185
Julieta Aguilera <aguilerajulieta700@gmail.com>
2023-11-14 21:51:18 -0300

    se agregan nuevos cambios

--- Julieta Aguilera/python/leccion4/Persona2.py
+++ Julieta Aguilera/python/leccion4/Persona2.py
@@ -1,5 +1,5 @@
 class Persona2:
-    def __int__(self, nombre, apellido, edad):
+    def __init__(self, nombre, apellido, edad):
         self._nombre = nombre
         self._apellido = apellido
         self._edad = edad
@@ -10,7 +10,7 @@ class Persona2:
     @property #decorador
     def nombre(self):#Metodo Getter
         print('Estamos usando metodo get')
-        return self._nombre()
+        return self._nombre
 
     @nombre.setter
     def nombre(self, nombre):
@@ -29,9 +29,17 @@ class Persona2:
     def edad(self):
         return self._edad
 
-    @edad.setter
-    def edad(self, edad):
-        self._edad
-
-#persona1 = Persona2('Julieta', 'Aguilera', 20)
-#print(persona1.nombre())
+   # @edad.setter
+   # def edad(self, edad):
+   #     self._edad = edad
+print('Ejecutando el programa')
+persona1 = Persona2('julieta', 'Aguilera', 20)
+print('clase instanciada')
+nombre = persona1.nombre()
+print('persona cargada en variable')
+print(nombre)
+#persona1.nombre = 'Juan Pedro' #Lamamos el metodo setter
+#print(persona1.nombre) # otra vez con el metodod getter
+#print(persona1.mostrar detalles()) #Lamamos el metodo mostrar detalles
+# Atributo read-only seria la edad porque no tiene el metodo set
+#print(persona1.edad)
